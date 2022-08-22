5ccfaa6d2445f17aca3936e6128f662eb332151f
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-08-22 21:18:55 +0530

    formatting

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,57 @@
-import React,{useReducer,createContext} from 'react';
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Dashboard from './components/Dashboard';
-import Error from './components/error';
-import Logout from './components/Users/Logout'
-import Login from './components/Users/Login';
-import Signup from './components/Users/Signup';
-import { initialState,reducer } from './reducer/UseReducer';
-import Showadmins from './components/ShowAdmins';
-import AddEmployee from './components/AddEmployee';
-import EditEmployee from './components/EditEmployee';
-import EditProject from './components/EditProject';
-import ShowEmployee from './components/ShowEmployee';
-import AddProject from './components/AddProject';
-import ShowProject from './components/ShowProject';
-import AddUser from './components/AddUser';
-import AssignProject from './components/AssignProject';
-import ShowAssignedproject from './components/ShowAssignedproject';
- 
+import React, { useReducer, createContext } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Error from "./components/error";
+import Logout from "./components/Users/Logout";
+import Login from "./components/Users/Login";
+import Signup from "./components/Users/Signup";
+import { initialState, reducer } from "./reducer/UseReducer";
+import Showadmins from "./components/ShowAdmins";
+import AddEmployee from "./components/AddEmployee";
+import EditEmployee from "./components/EditEmployee";
+import EditProject from "./components/EditProject";
+import ShowEmployee from "./components/ShowEmployee";
+import AddProject from "./components/AddProject";
+import ShowProject from "./components/ShowProject";
+import AddUser from "./components/AddUser";
+import AssignProject from "./components/AssignProject";
+import ShowAssignedproject from "./components/ShowAssignedproject";
+
 export const UserContext = createContext();
 
 const App = () => {
-     const [state,dispatch] = useReducer(reducer,initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <>
-    
-    <UserContext.Provider value={{state, dispatch}}>
-    <Router>
-    <Routes>    
-    <Route path="/" element={<Login />}></Route>
-    <Route path="/register" element={<Signup />}></Route>
+      <UserContext.Provider value={{ state, dispatch }}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />}></Route>
+            <Route path="/register" element={<Signup />}></Route>
 
-    <Route path="/dashboard" element={<Dashboard />}></Route>    
-    <Route path="/admins" element={<Showadmins />}></Route>
-    <Route path="/showemployee" element={<ShowEmployee />}></Route>
-    <Route path="/showproject" element={<ShowProject />}></Route>
-    <Route path="/showassignedproject" element={<ShowAssignedproject />}></Route>
-    <Route path="/adduser" element={<AddUser />}></Route>
-    <Route path="/addemployee" element={<AddEmployee />}></Route>
-    <Route path="/addproject" element={<AddProject />}></Route>
-    <Route path="/assignproject" element={<AssignProject />}></Route>
-    <Route path="/editemployee/:id" element={<EditEmployee />}></Route>
-    <Route path="/editproject/:id" element={<EditProject />}></Route>
-    <Route path="/logout" element={<Logout />}></Route>
+            <Route path="/dashboard" element={<Dashboard />}></Route>
+            <Route path="/admins" element={<Showadmins />}></Route>
+            <Route path="/showemployee" element={<ShowEmployee />}></Route>
+            <Route path="/showproject" element={<ShowProject />}></Route>
+            <Route
+              path="/showassignedproject"
+              element={<ShowAssignedproject />}
+            ></Route>
+            <Route path="/adduser" element={<AddUser />}></Route>
+            <Route path="/addemployee" element={<AddEmployee />}></Route>
+            <Route path="/addproject" element={<AddProject />}></Route>
+            <Route path="/assignproject" element={<AssignProject />}></Route>
+            <Route path="/editemployee/:id" element={<EditEmployee />}></Route>
+            <Route path="/editproject/:id" element={<EditProject />}></Route>
+            <Route path="/logout" element={<Logout />}></Route>
 
-
-
-    <Route path="*" element={<Error />} />
-    </Routes>
-  
-    </Router>
-    </UserContext.Provider> 
-      
-    
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Router>
+      </UserContext.Provider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,177 +1,167 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Header from './header/Header';
-import { NavLink } from 'react-router-dom';
-import { UserContext } from '../App';
+import React, { useState, useEffect, useContext } from "react";
+import Header from "./header/Header";
+import { NavLink } from "react-router-dom";
+import { UserContext } from "../App";
 // import { FaUsers } from 'react-icons/fa';
 // import { BsInfoLg } from 'react-icons/bs';
-import Sidenavbar from './Sidenavbar'
-import Topnavbar from './Topnavbar';
-import Footer from './Footer';
+import Sidenavbar from "./Sidenavbar";
+import Topnavbar from "./Topnavbar";
+import Footer from "./Footer";
 
 const Dashboard = () => {
+  const [userName, setUserName] = useState("");
+  const [show, setShow] = useState(false);
 
-    const [userName, setUserName] = useState('');
-    const [show, setShow] = useState(false);
+  const userHomePage = async () => {
+    try {
+      const res = await fetch("/getdata", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+      setUserName(data.name);
+      setShow(true);
 
-    const userHomePage = async () => {
-        try {
-            const res = await fetch('/getdata', {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-            })
-            const data = await res.json();
-            console.log(data);
-            setUserName(data.name);
-            setShow(true);
-
-            // if(!res.status===200){
-            //     const error = new Error(res.error)
-            //     throw error;
-            // }
-        } catch (error) {
-            console.log(error);
-        }
+      // if(!res.status===200){
+      //     const error = new Error(res.error)
+      //     throw error;
+      // }
+    } catch (error) {
+      console.log(error);
     }
-    useEffect(() => {
-        userHomePage();
-    }, []);
+  };
+  useEffect(() => {
+    userHomePage();
+  }, []);
 
+  return (
+    <>
+      <div id="wrapper">
+        <Sidenavbar />
 
-    return (
-        <>
+        <div id="content-wrapper" className="d-flex flex-column">
+          <div id="content">
+            <Topnavbar />
 
-            <div id="wrapper">
-
-                <Sidenavbar />
-
-
-                <div id="content-wrapper" className="d-flex flex-column">
-
-
-                    <div id="content">
-
-                        <Topnavbar />
-
-                        <div className="container-fluid">
-
-
-                            <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                                <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
-                                {/* <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+            <div className="container-fluid">
+              <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
+                {/* <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                     className="fas fa-download fa-sm text-white-50"></i> Generate Report</a> */}
-                            </div>
+              </div>
 
-
-                            <div className="row">
-
-
-                                <div className="col-xl-3 col-md-6 mb-4">
-                                    <div className="card border-left-primary shadow h-100 py-2">
-                                        <div className="card-body">
-                                            <div className="row no-gutters align-items-center">
-                                                <div className="col mr-2">
-                                                    <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                        Earnings (Monthly)</div>
-                                                    <div className="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                                                </div>
-                                                <div className="col-auto">
-                                                    <i className="fas fa-calendar fa-2x text-gray-300"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                                <div className="col-xl-3 col-md-6 mb-4">
-                                    <div className="card border-left-success shadow h-100 py-2">
-                                        <div className="card-body">
-                                            <div className="row no-gutters align-items-center">
-                                                <div className="col mr-2">
-                                                    <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                        Earnings (Annual)</div>
-                                                    <div className="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-                                                </div>
-                                                <div className ="col-auto">
-                                                    <i className ="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                                <div className="col-xl-3 col-md-6 mb-4">
-                                    <div className="card border-left-info shadow h-100 py-2">
-                                        <div className="card-body">
-                                            <div className="row no-gutters align-items-center">
-                                                <div className="col mr-2">
-                                                    <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-                                                    </div>
-                                                    <div className="row no-gutters align-items-center">
-                                                        <div className="col-auto">
-                                                            <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                                                        </div>
-                                                        <div className="col">
-                                                            <div className="progress progress-sm mr-2">
-                                                                <div className="progress-bar bg-info" role="progressbar"
-                                                                    style={{ width: "50%" }} aria-valuenow="50" aria-valuemin="0"
-                                                                    aria-valuemax="100"></div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className ="col-auto">
-                                                    <i className ="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                                <div className ="col-xl-3 col-md-6 mb-4">
-                                    <div className ="card border-left-warning shadow h-100 py-2">
-                                        <div className ="card-body">
-                                            <div className ="row no-gutters align-items-center">
-                                                <div className ="col mr-2">
-                                                    <div className ="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                        Pending Requests</div>
-                                                    <div className ="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                                                </div>
-                                                <div className ="col-auto">
-                                                    <i className ="fas fa-comments fa-2x text-gray-300"></i>
-                                                </div>
-                                            </div>Name
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
+              <div className="row">
+                <div className="col-xl-3 col-md-6 mb-4">
+                  <div className="card border-left-primary shadow h-100 py-2">
+                    <div className="card-body">
+                      <div className="row no-gutters align-items-center">
+                        <div className="col mr-2">
+                          <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                            Earnings (Monthly)
+                          </div>
+                          <div className="h5 mb-0 font-weight-bold text-gray-800">
+                            $40,000
+                          </div>
                         </div>
-
-
+                        <div className="col-auto">
+                          <i className="fas fa-calendar fa-2x text-gray-300"></i>
+                        </div>
+                      </div>
                     </div>
-                    <Footer />
+                  </div>
                 </div>
 
+                <div className="col-xl-3 col-md-6 mb-4">
+                  <div className="card border-left-success shadow h-100 py-2">
+                    <div className="card-body">
+                      <div className="row no-gutters align-items-center">
+                        <div className="col mr-2">
+                          <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
+                            Earnings (Annual)
+                          </div>
+                          <div className="h5 mb-0 font-weight-bold text-gray-800">
+                            $215,000
+                          </div>
+                        </div>
+                        <div className="col-auto">
+                          <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
 
+                <div className="col-xl-3 col-md-6 mb-4">
+                  <div className="card border-left-info shadow h-100 py-2">
+                    <div className="card-body">
+                      <div className="row no-gutters align-items-center">
+                        <div className="col mr-2">
+                          <div className="text-xs font-weight-bold text-info text-uppercase mb-1">
+                            Tasks
+                          </div>
+                          <div className="row no-gutters align-items-center">
+                            <div className="col-auto">
+                              <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                                50%
+                              </div>
+                            </div>
+                            <div className="col">
+                              <div className="progress progress-sm mr-2">
+                                <div
+                                  className="progress-bar bg-info"
+                                  role="progressbar"
+                                  style={{ width: "50%" }}
+                                  aria-valuenow="50"
+                                  aria-valuemin="0"
+                                  aria-valuemax="100"
+                                ></div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-auto">
+                          <i className="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-xl-3 col-md-6 mb-4">
+                  <div className="card border-left-warning shadow h-100 py-2">
+                    <div className="card-body">
+                      <div className="row no-gutters align-items-center">
+                        <div className="col mr-2">
+                          <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                            Pending Requests
+                          </div>
+                          <div className="h5 mb-0 font-weight-bold text-gray-800">
+                            18
+                          </div>
+                        </div>
+                        <div className="col-auto">
+                          <i className="fas fa-comments fa-2x text-gray-300"></i>
+                        </div>
+                      </div>
+                      Name
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            
-            <a className="scroll-to-top rounded" href="#page-top">
-                <i className="fas fa-angle-up"></i>
-            </a>
+          </div>
+          <Footer />
+        </div>
+      </div>
 
+      <a className="scroll-to-top rounded" href="#page-top">
+        <i className="fas fa-angle-up"></i>
+      </a>
+    </>
+  );
+};
 
-
-
-
-        </>
-    )
-}
-
-export default Dashboard
-
-
+export default Dashboard;
