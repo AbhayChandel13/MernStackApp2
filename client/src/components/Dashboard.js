d663edbbcc87748cb5c552ba3ff07efcd7e63d2f
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-09-10 04:09:13 +0530

    not showing role on dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -57,7 +57,7 @@ const Dashboard = () => {
 
             <div className="container-fluid">
               <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 className="h3 mb-0 text-gray-800">Welcome Back {userName.name} ({userName.role}) </h1>
+                <h1 className="h3 mb-0 text-gray-800">Welcome Back {userName.name} </h1>
                 {/* <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                     className="fas fa-download fa-sm text-white-50"></i> Generate Report</a> */}
               </div>
