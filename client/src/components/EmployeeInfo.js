e6aeff8bf01bbfcebfc4992f1f649eb87fdedec5
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-09-15 03:25:13 +0530

    Formatting done

--- client/src/components/EmployeeInfo.js
+++ client/src/components/EmployeeInfo.js
@@ -1,26 +1,25 @@
-import React,{useState,useEffect} from 'react';
-import Sidenavbar from './Sidenavbar';
-import Topnavbar from './Topnavbar';
-import Footer from './Footer';
-import { HiUserAdd } from 'react-icons/hi';
-import { NavLink, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Sidenavbar from "./Sidenavbar";
+import Topnavbar from "./Topnavbar";
+import Footer from "./Footer";
+import { HiUserAdd } from "react-icons/hi";
+import { NavLink, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const EmployeeInfo = () => {
-    let navigate = useNavigate();
-    let [employeedata, setEmployeedata] = useState([]);
-   
-    const [employeeinfo, setEmployeeinfo] = useState({
-       empid: "",
-        file1: "",
-        file2: "",
-        file3: ""       
-        
-    });
- //   const [role, setRole] = useState("");
+  let navigate = useNavigate();
+  let [employeedata, setEmployeedata] = useState([]);
 
- const getEmployees = async (e) => {
+  const [employeeinfo, setEmployeeinfo] = useState({
+    empid: "",
+    file1: "",
+    file2: "",
+    file3: "",
+  });
+  //   const [role, setRole] = useState("");
+
+  const getEmployees = async (e) => {
     try {
       const res = await fetch("/api/v1/employees/getEmployee");
 
@@ -41,83 +40,73 @@ const EmployeeInfo = () => {
     getEmployees();
   }, []);
 
-    let name, value;
-    const handleInputs = (e) => {
-        console.log(e);
-        name = e.target.name;
-        value = e.target.value;
+  let name, value;
+  const handleInputs = (e) => {
+    console.log(e);
+    name = e.target.name;
+    value = e.target.value;
 
-        setEmployeeinfo({ ...employeeinfo, [name]: value })
+    setEmployeeinfo({ ...employeeinfo, [name]: value });
+  };
+
+  const PostData = async (e) => {
+    e.preventDefault();
+
+    const { empid, file1, file2, file3 } = employeeinfo;
+    //const {roleid} = role;
+
+    let res = await fetch("/employee", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ empid, file1, file2, file3 }),
+    });
+
+    const data = await res.json();
+
+    if (res.status === 422 || !data) {
+      //window.alert("Invalid Registration");
+      toast.error(" Invalid Employee Data", {
+        position: "top-center",
+      });
+      console.log("Invalid Employee Data");
+    } else {
+      toast.success("Employee Added Successfully!", {
+        position: "top-center",
+      });
+      console.log("Employee Added Successful");
+
+      setTimeout(function () {
+        navigate("/showemployee", { replace: true });
+      }, 2000);
     }
-
-    const PostData = async (e) => {
-        e.preventDefault();
-
-        const {empid,file1,file2,file3} = employeeinfo;
-        //const {roleid} = role;
-        
-        let res = await fetch("/employee", {
-            method: "POST",
-            headers: { "Content-Type": "application/json", },
-            body: JSON.stringify({empid,file1,file2,file3}),
-        });
-
-     
-
-        const data = await res.json();       
-
-        if (res.status === 422 || !data) {
-            //window.alert("Invalid Registration");
-            toast.error(" Invalid Employee Data", {
-                position: "top-center",
-            });
-            console.log("Invalid Employee Data");
-        }
-        else {
-            toast.success("Employee Added Successfully!", {
-                position: "top-center",
-            });
-            console.log("Employee Added Successful");
-
-            setTimeout(function () {
-                navigate("/showemployee", { replace: true });
-              }, 2000);
-            
-        }
-
-    }
-
-   
+  };
 
   return (
     <>
       <div id="wrapper">
+        <Sidenavbar />
 
-<Sidenavbar />
+        <div id="content-wrapper" className="d-flex flex-column">
+          <div id="content">
+            <ToastContainer />
 
+            <Topnavbar />
 
-<div id="content-wrapper" className="d-flex flex-column">
-
-
-    <div id="content">
-    <ToastContainer />
-
-        <Topnavbar />
-
-        <div className="container-fluid">        
-        <div className="card shadow mb-4"> 
-        <div className="card-header py-3 ">
-                            <h6 className="m-0 font-weight-bold text-primary"><HiUserAdd />  Employee Information</h6>
-                        </div>
-                        <div className="card-body">
-        <form className="user mt-2 p-3 d-flex flex-column justify-content-center align-items-center">
-      
-        <div className="form-group col-sm-8" id="empname">
+            <div className="container-fluid">
+              <div className="card shadow mb-4">
+                <div className="card-header py-3 ">
+                  <h6 className="m-0 font-weight-bold text-primary">
+                    <HiUserAdd /> Employee Information
+                  </h6>
+                </div>
+                <div className="card-body">
+                  <form className="user mt-2 p-3 d-flex flex-column justify-content-center align-items-center">
+                    <div className="form-group col-sm-8" id="empname">
                       <select
                         id="empid"
                         name="empid"
                         style={{ borderRadius: "30px" }}
-                        className="custom-select custom-select-lg col-lg-12"                        
+                        className="custom-select custom-select-lg col-lg-12"
                         value={employeeinfo.empid}
                         onChange={handleInputs}
                       >
@@ -134,70 +123,71 @@ const EmployeeInfo = () => {
                         ))}
                       </select>
                     </div>
-                        
-                      
-                        <div className="form-group col-sm-8 ">
-                                <input 
-                                type="file"
-                                name="file1"
-                                className="form-control input-lg  form-control-user "
-                                id="File1"
-                                placeholder="Upload File "
-                                value={employeeinfo.file1}
-                                onChange={handleInputs}
-                                />                        
-                        </div>                     
-                        
-                        <div className="form-group col-sm-8 ">
-                                <input 
-                                type="file"
-                                name="file2"
-                                className="form-control input-lg  form-control-user "
-                                id="File2"
-                                placeholder="Upload File "
-                                value={employeeinfo.file2}
-                                onChange={handleInputs}
-                                />                        
-                        </div>   
-                      
-                        
-                        <div className="form-group col-sm-8 ">
-                                <input 
-                                type="file"
-                                name="file3"
-                                className="form-control input-lg  form-control-user"
-                                id="File3"
-                                placeholder="Upload File "
-                                value={employeeinfo.file3}
-                                onChange={handleInputs}
-                                />                        
-                        </div>  
 
+                    <div className="form-group col-sm-8 ">
+                      <input
+                        type="file"
+                        name="file1"
+                        className="form-control input-lg  form-control-user "
+                        id="File1"
+                        placeholder="Upload File "
+                        value={employeeinfo.file1}
+                        onChange={handleInputs}
+                      />
+                    </div>
 
-                <div className="mt-4 mb-0">
-                <div className="d-grid"><NavLink className="btn btn-primary btn-user btn-block" to="/" name="signup" id="signup" value="register" onClick={PostData}>Upload Files</NavLink></div>
-                 </div> 
-                       
-                     
-         </form>
-         </div>
-         </div>
+                    <div className="form-group col-sm-8 ">
+                      <input
+                        type="file"
+                        name="file2"
+                        className="form-control input-lg  form-control-user "
+                        id="File2"
+                        placeholder="Upload File "
+                        value={employeeinfo.file2}
+                        onChange={handleInputs}
+                      />
+                    </div>
+
+                    <div className="form-group col-sm-8 ">
+                      <input
+                        type="file"
+                        name="file3"
+                        className="form-control input-lg  form-control-user"
+                        id="File3"
+                        placeholder="Upload File "
+                        value={employeeinfo.file3}
+                        onChange={handleInputs}
+                      />
+                    </div>
+
+                    <div className="mt-4 mb-0">
+                      <div className="d-grid">
+                        <NavLink
+                          className="btn btn-primary btn-user btn-block"
+                          to="/"
+                          name="signup"
+                          id="signup"
+                          value="register"
+                          onClick={PostData}
+                        >
+                          Upload Files
+                        </NavLink>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Footer />
         </div>
+      </div>
 
-
-    </div>
-    <Footer />
-</div>
-
-
-</div>
-
-<a className="scroll-to-top rounded" href="#page-top">
-<i className="fas fa-angle-up"></i>
-</a>
-
+      <a className="scroll-to-top rounded" href="#page-top">
+        <i className="fas fa-angle-up"></i>
+      </a>
     </>
-  )
-}
+  );
+};
 
 export default EmployeeInfo;
