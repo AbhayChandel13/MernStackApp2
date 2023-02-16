a76eb17796154e7b359f6c2fe2c7e5a4e4ec3ce2
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-02-17 03:43:35 +0530

    code format

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -93,55 +93,64 @@ const Dashboard = () => {
 
   const data = [
     {
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
       caption: `<div>
                   San Francisco
                   <br/>
                   Next line
-                </div>`
+                </div>`,
     },
     {
-      image: "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
-      caption: "Scotland"
+      image:
+        "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
+      caption: "Scotland",
     },
     {
-      image: "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
-      caption: "Darjeeling"
+      image:
+        "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
+      caption: "Darjeeling",
     },
     {
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
-      caption: "San Francisco"
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
+      caption: "San Francisco",
     },
     {
-      image: "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
-      caption: "Scotland"
+      image:
+        "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
+      caption: "Scotland",
     },
     {
-      image: "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
-      caption: "Darjeeling"
+      image:
+        "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
+      caption: "Darjeeling",
     },
     {
-      image: "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
-      caption: "San Francisco"
+      image:
+        "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
+      caption: "San Francisco",
     },
     {
-      image: "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
-      caption: "Scotland"
+      image:
+        "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
+      caption: "Scotland",
     },
     {
-      image: "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
-      caption: "Darjeeling"
-    }
+      image:
+        "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
+      caption: "Darjeeling",
+    },
   ];
 
   const captionStyle = {
-    fontSize: '2em',
-    fontWeight: 'bold',
-  }
+    fontSize: "2em",
+    fontWeight: "bold",
+  };
   const slideNumberStyle = {
-    fontSize: '20px',
-    fontWeight: 'bold',
-  }
+    fontSize: "20px",
+    fontWeight: "bold",
+  };
 
   const breakPoints = [
     { width: 1, itemsToShow: 1 },
@@ -283,8 +292,7 @@ const Dashboard = () => {
                 </div>
               </div>
 
-
-                   {/* <div style={{ textAlign: "center" }}>
+              {/* <div style={{ textAlign: "center" }}>
                     <h2>React Carousel Minimal</h2>
                     <p>Easy to use, responsive and customizable carousel component for React Projects.</p>
                     <div style={{
@@ -318,25 +326,31 @@ const Dashboard = () => {
                   </div>
                   </div> */}
 
-<h1 style={{ textAlign: "center" }}>Example to setup your carousel in react</h1>
-      <div className="App">
-        <Carousel breakPoints={breakPoints}>
-          {/* {data.map(data=><div key={data.title}>{data.image}</div>)} */}
-          {/* <div style={containerStyles}>
+              <h1 style={{ textAlign: "center" }}>
+                Example to setup your carousel in react
+              </h1>
+              <div className="App">
+                <Carousel breakPoints={breakPoints}>
+                  {/* {data.map(data=><div key={data.title}>{data.image}</div>)} */}
+                  {/* <div style={containerStyles}>
                   <ImageSlider slides={slides} />
                 </div> */}
-          <Item><ImageSlider slides={slides}></ImageSlider></Item>
-          <Item><ImageSlider slides={slides}></ImageSlider></Item>
-          <Item><ImageSlider slides={slides}></ImageSlider></Item>
-          <Item>Four</Item>
-          <Item>Five</Item>
-          <Item>Six</Item>
-          <Item>Seven</Item>
-          <Item>Eight</Item> 
-        </Carousel>
-      </div>
-
-
+                  <Item>
+                    <ImageSlider slides={slides}></ImageSlider>
+                  </Item>
+                  <Item>
+                    <ImageSlider slides={slides}></ImageSlider>
+                  </Item>
+                  <Item>
+                    <ImageSlider slides={slides}></ImageSlider>
+                  </Item>
+                  <Item>Four</Item>
+                  <Item>Five</Item>
+                  <Item>Six</Item>
+                  <Item>Seven</Item>
+                  <Item>Eight</Item>
+                </Carousel>
+              </div>
             </div>
           </div>
           <Footer />
