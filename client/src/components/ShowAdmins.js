bc8777d493705589651d6beff1b6a227665e4dbe
AbhayChandel13 <chandelabhay4515@gmail.com>
2024-01-11 20:03:01 +0530

    comment removed

--- client/src/components/ShowAdmins.js
+++ client/src/components/ShowAdmins.js
@@ -44,7 +44,7 @@ const Showadmins = () => {
   //comment 1 
   //comment 2
   //comment 1 
-  //comment 2
+  
   //check local storage
   // useEffect(() => {
   //   // const data = window.localStorage.getItem('MY_APP_STATE');
